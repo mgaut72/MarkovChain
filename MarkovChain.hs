d1e774882dac35483c12c960c28b5bf2e7fe45bb
Matt Gautreau <mgaut72@gmail.com>
2014-05-19 20:47:02 -0700

    add creation function for a chain graph

--- MarkovChain.hs
+++ MarkovChain.hs
@@ -3,10 +3,12 @@
 module MarkovChain where
 
 import Data.Monoid
-import Data.List (foldl')
+import Data.List
+import Data.Maybe
+import qualified Data.Map as Map
 import Data.Graph.Inductive
 
-type MarkovChain a = Gr (a, Int) Int
+type MarkovChain a = Gr (a) Int
 
 instance Monoid (MarkovChain a) where
   mempty  = empty
@@ -17,3 +19,25 @@ addChains mc1 mc2 = foldr (&) mc1 newContexts
         newContexts = zipWith changeNodeNumber oldContexts openNodeNumbers
         changeNodeNumber (a,_,b,c) x = (a, x, b, c)
         openNodeNumbers = newNodes (length oldContexts) mc1
+
+markovChain :: (Eq a) => [a] -> MarkovChain a
+markovChain xs = mkGraph ns es
+ where ns = makeNodes xs
+       es = makeEdges ns xs
+
+makeNodes :: (Eq a) => [a] -> [LNode a]
+makeNodes xs = zip [0..] (nub xs)
+
+makeEdges ns xs = map flatten (Map.toList m)
+ where flatten ((a,b),c) = (a,b,c)
+       m = makeEdgesH ns xs
+
+makeEdgesH :: (Eq a) => [LNode a] -> [a] -> Map.Map (Node, Node) Int
+makeEdgesH ns [_]      = Map.empty
+makeEdgesH ns (x:y:ys) = Map.insertWith' (+) (n1,n2) 1 m
+ where m = makeEdgesH ns (y:ys)
+       n1 = lookupNode ns x
+       n2 = lookupNode ns y
+
+lookupNode nodes label = n
+ where (n,_) = fromJust $ find (\(_,l) -> l == label) nodes
