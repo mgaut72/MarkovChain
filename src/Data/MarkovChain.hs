9488bf79c469833676eb73ab89d01f902574293d
Matt Gautreau <mgaut72@gmail.com>
2014-05-23 19:34:11 -0700

    use a seq rather than a union which has to constantly be converted to a list

--- src/Data/MarkovChain.hs
+++ src/Data/MarkovChain.hs
@@ -8,21 +8,20 @@ module Data.MarkovChain
   ) where
 
 import Data.Monoid
-import Data.List
 import Control.Monad.State
 import Data.Maybe
 import System.Random
 import Data.Random
 import Data.Random.Source
 import Data.Random.Extras
+import Data.Seq
 import qualified Data.Map as Map
-import qualified Data.MultiSet as MSet
 
-type MarkovChain a = Map.Map a (MSet.MultiSet a)
+type MarkovChain a = Map.Map a (Seq.Seq a)
 
 instance Ord a => Monoid (MarkovChain a) where
   mempty  = Map.empty
-  mappend = Map.unionWith MSet.union
+  mappend = Map.unionWith mappend
 
 --instance (Show a) => Show (MarkovChain a) where
 --  show mc = undefined
@@ -32,23 +31,20 @@ instance Ord a => Monoid (MarkovChain a) where
 --
 markovChain :: (Ord a) => [a] -> MarkovChain a
 markovChain []     = Map.empty
-markovChain [x]    = Map.singleton x MSet.empty
-markovChain (x:y:xs) = Map.insertWith' MSet.union x (MSet.singleton y) (markovChain (y:xs))
+markovChain [x]    = Map.singleton x empty
+markovChain (x:y:xs) = Map.insertWith' mappend x (singleton y) (markovChain (y:xs))
 
 --
 -- Random traversal of the data structure
 --
 traverse :: Ord a => StdGen -> MarkovChain a -> Int -> [a]
 traverse g mc n = fst $ traverseH g' mc n start
-  where (start, g') = rChoice g $ Map.keys mc
+  where (start, g')  = rChoice g $ Map.keys mc
+        rChoice g xs = runState (runRVar (choice xs) StdRandom) g
 
 traverseH :: Ord a => StdGen -> MarkovChain a -> Int -> a -> ([a], StdGen)
 traverseH g mc 0 a = ([a], g)
 traverseH g mc n a = (a:as, g'')
-  where (as, g'') = traverseH g' mc (n-1) nextA
-        potential = MSet.toList . fromJust $ Map.lookup a mc
-        (idx, g') = randomR (0, length potential) g
-        nextA     = potential !! idx
-
-rChoice :: StdGen -> [a] -> (a, StdGen)
-rChoice g xs = runState (runRVar (choice xs) StdRandom) g
+  where (as, g'')  = traverseH g' mc (n-1) nextA
+        (nxtA, g') = rChoice g $ fromJust $ Map.lookup a mc
+        rChoice g xs = runState (runRVar (choiceSeq xs) StdRandom) g
