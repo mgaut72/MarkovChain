8aa115df2b11523178953d65c7a19a530f7e1b2b
Matt Gautreau <mgaut72@gmail.com>
2014-06-17 21:30:29 -0700

    it works!

--- markov-bot/Web/MarkovBot/Main.hs
+++ markov-bot/Web/MarkovBot/Main.hs
@@ -2,29 +2,27 @@ module Web.MarkovBot.Main where
 import Data.List
 import Network
 import System.IO
-import System.Time
 import System.Exit
 import Control.Monad.State
 import Control.Exception
 import Text.Printf
 import Prelude hiding (catch)
 import Data.Monoid
-
 import Data.MarkovChain
 
 server = "irc.freenode.org"
 port   = 6667
 chan   = "#tutbot-testing"
 nick   = "MarkovBot"
+ownerNick = "mgaut72"
 
 --
 -- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
--- A socket and the bot's start time.
+-- A socket and the markov chain structure
 --
 type Net = StateT Bot IO
 data Bot = Bot { socket :: Handle
-               , starttime :: ClockTime
-               , markovChain :: MarkovChain String
+               , mChain :: MarkovChain String
                }
 
 --
@@ -43,10 +41,9 @@ main = bracket connect disconnect loop
 --
 connect :: IO Bot
 connect = notify $ do
-    t <- getClockTime
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    return (Bot h t mempty)
+    return (Bot h mempty)
   where
     notify a = bracket_
         (printf "Connecting to %s ... " server >> hFlush stdout)
@@ -65,9 +62,6 @@ run = do
     bot <- get
     listen $ socket bot
 
-
-
-
 --
 -- Process each line from the server
 --
@@ -75,19 +69,33 @@ listen :: Handle -> Net ()
 listen h = forever $ do
     s <- init `fmap` io (hGetLine h)
     io (putStrLn s)
-    eval (clean s)
+    if ping s
+       then pong s
+       else if (':':ownerNick) `isPrefixOf` s then eval (clean s) else return ()
   where
     forever a = a >> forever a
     clean     = drop 1 . dropWhile (/= ':') . drop 1
+    ping x    = "PING :" `isPrefixOf` x
+    pong x    = write "PONG" (':' : drop 6 x)
 
 --
 -- Dispatch a command
 --
 eval :: String -> Net ()
-eval     "!MBuptime"             = uptime >>= privmsg
 eval     "!MBquit"               = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval     "!MBspeak"              = speak
 eval x | "!MBid " `isPrefixOf` x = privmsg (drop 6 x)
-eval      _                      = return () -- ignore everything else
+eval x                           = expandVocabulary x
+
+expandVocabulary :: String -> Net ()
+expandVocabulary x = modify updateBot
+  where updateBot b = b { mChain = mappend newChain (mChain b) }
+        newChain = markovChain $ words x
+
+speak :: Net ()
+speak = do
+  bot <- get
+  privmsg $ concat $ traverse' (mChain bot) 15
 
 --
 -- Send a privmsg to the current chan + server
@@ -104,30 +112,6 @@ write s t = do
     let h = socket bot
     io $ hPrintf h "%s %s\r\n" s t
     io $ printf    "> %s %s\n" s t
-
---
--- Calculate and pretty print the uptime
---
-uptime :: Net String
-uptime = do
-    now  <- io getClockTime
-    bot <- get
-    return . pretty $ diffClockTimes now (starttime bot)
-
---
--- Pretty print the date in '1d 9h 9m 17s' format
---
-pretty :: TimeDiff -> String
-pretty td = join . intersperse " " . filter (not . null) . map f $
-    [(years          ,"y") ,(months `mod` 12,"m")
-    ,(days   `mod` 28,"d") ,(hours  `mod` 24,"h")
-    ,(mins   `mod` 60,"m") ,(secs   `mod` 60,"s")]
-  where
-    secs    = abs $ tdSec td  ; mins   = secs   `div` 60
-    hours   = mins   `div` 60 ; days   = hours  `div` 24
-    months  = days   `div` 28 ; years  = months `div` 12
-    f (i,s) | i == 0    = []
-            | otherwise = show i ++ s
 
 --
 -- Convenience.
